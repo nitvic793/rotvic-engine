c509726a6e680ef0e566aade3127c3f8237fcb37
Nitish Victor <nithishvictor@gmail.com>
2018-01-24 22:53:02 -0500

    Core Game Loop Test

--- Egnine.Tests/EngineTests.cpp
+++ Egnine.Tests/EngineTests.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include "../Engine/CoreGame.h"
 #include "../Engine/SystemCore.h"
+#include "../Engine/IGame.h"
 #include "../FakeIt/single_header/mstest/fakeit.hpp"
 
 using namespace fakeit;
@@ -11,7 +12,19 @@ namespace EngineTests
 {		
 	TEST_CLASS(CoreGameTests)
 	{
+		CoreGame *game;
 	public:
+
+		TEST_METHOD_INITIALIZE(Test_Initialization)
+		{
+			CoreGame *game = new CoreGame(1280, 720, "DXGame");
+			HINSTANCE hInstance = 0; //Fake HINSTANCE		
+			SystemCore &core = *game->GetSystemCore();
+			Mock<SystemCore> coreMock(core);
+			Fake(Method(coreMock, InitializeWindow));
+			Fake(Method(coreMock, InitializeAndBindDirectX));
+			game->Initialize(hInstance, 0);
+		}
 		
 		TEST_METHOD(CoreGame_Initialization)
 		{
@@ -24,6 +37,23 @@ namespace EngineTests
 			Assert::IsTrue(game->Initialize(hInstance, 0));		
 			Verify(Method(coreMock, InitializeWindow));
 			Verify(Method(coreMock, InitializeAndBindDirectX));
+			delete game;
+		}		
+
+		TEST_METHOD(CoreGame_Run)
+		{
+			Mock<CoreGame> gameMock(*game);
+			Mock<IGame> gameInstanceMock;
+			When(Method(gameInstanceMock, Update)).Do([]() {
+				game->SetState(Quit);
+			});
+			game->Bind(&gameInstanceMock.get());
+			game->Run();
+			Assert::AreEqual(Quit, game->GetState());
+		}
+
+		TEST_METHOD_CLEANUP(Test_Cleanup)
+		{
 			delete game;
 		}
 
