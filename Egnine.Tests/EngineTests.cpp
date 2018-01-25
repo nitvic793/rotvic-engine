f511eaf6908b6f26a03e6d8ad36392f2fe0beb3a
Nitish Victor <nithishvictor@gmail.com>
2018-01-24 21:51:35 -0500

    Core Engine Window init test

--- Egnine.Tests/EngineTests.cpp
+++ Egnine.Tests/EngineTests.cpp
@@ -6,13 +6,14 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace EngineTests
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(CoreGameTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(CoreGame_Initialization)
 		{
-			
+			CoreGame *game = new CoreGame(1280, 720, "DXGame");
+			Assert::IsTrue(game->InitializeWindow());
 		}
 
 	};
